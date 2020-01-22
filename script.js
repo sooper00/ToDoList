68be387132b7c1073312545626f056035aad7946
sooper00 <kms6088@gmail.com>
2020-01-22 13:37:47 +0000

    Complete Check-Mark

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const addButton = document.querySelector('.add-button');
 const input = document.querySelector('.add-section').querySelector('input');
 const list = document.querySelector('.list-wrapper');
+const item = document.querySelector('.list-item');
 
 function deletion(event) {
   const targetTag = event.target.parentNode;
@@ -8,28 +9,46 @@ function deletion(event) {
   targetTag.parentNode.removeChild(targetTag);
 }
 
+function checkHandler(event){
+  const targetTag = event.currentTarget;
+  const textC = targetTag.querySelector('.todo-text');
+  const checkC = targetTag.querySelector('.check-mark');
+
+  if(textC.classList[1] === 'checked') {
+    textC.classList.remove('checked');
+    checkC.innerHTML = '';
+  }
+  else {
+    textC.classList.add('checked');
+    checkC.innerHTML = '✓';
+  }
+}
+
 function creation() {
   const inputData = input.value;
   let child = document.createElement('div');
   let removeC = document.createElement('div');
   let textC = document.createElement('div');
+  let checkC = document.createElement('div');
   
   child.classList.add('list-item');
   removeC.classList.add('remove');
   textC.classList.add('todo-text');
+  checkC.classList.add('check-mark');
   
   removeC.innerHTML = '❌';
-  removeC.addEventListener('click', deletion);
   textC.innerHTML = inputData;
+  
+  removeC.addEventListener('click', deletion);
+  child.addEventListener('click', checkHandler);
 
-  child.appendChild(removeC);  
+  child.appendChild(removeC); 
   child.appendChild(textC);
-  list.appendChild(child);
+  child.appendChild(checkC);
+  list.appendChild(child);  
 
   input.value = '';
 }
-
-
 
 function enterHandler(event){
   if(event.keyCode == 13) {
